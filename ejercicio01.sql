9491afab6ad1c4f540d94e666518fd9e5c1efc8e
Academico <Academico@T08KETIPC18A202.instructores.senati.local>
2024-02-29 13:11:08 -0500

    ejercicio02

--- ejercicio01.sql
+++ ejercicio01.sql
@@ -2,9 +2,18 @@
 
 /*
 
-01. Listar todos los customers que inicien su id con la letra "A". Guardas el archivo como "ejercicio01.sql" y lo subes a tu repositorio
+01. Listar todos los customers que inicien su id con la letra "A". Guardas el archivo como "ejercicio01.sql" y lo subes a tu repositorio.
+02. Listar a todos los customers que sean de los países: France, Germany, Sweden. Guardas el archivo como "ejercicio01.sql" y lo subes a tu repositorio.
 
 */
 
 SELECT * FROM Customers
 WHERE CustomerID LIKE 'A%'
+
+
+SELECT * FROM Customers
+WHERE Country = 'France'
+OR 
+Country = 'Germany'
+OR
+Country = 'Sweden'
